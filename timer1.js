1c9a3e1eafeb7573e5ed9844a397062b6b7bd4fb
Japhet Elias <japhet9@gmail.com>
2021-09-23 20:41:05 +0000

    commented code for clarity

--- timer1.js
+++ timer1.js
@@ -1,10 +1,10 @@
 const args = process.argv.slice(2); //get the command line arguments (if any)
 
 if(args.length > 0) {
-  for (const time of args) {
+  for (const time of args) { //assuming time in seconds
     if(!isNaN(time) && time >= 0) {
       setTimeout(() => {
-        process.stdout.write('.');
+        process.stdout.write('.'); //'\x07' not used because system beep not working
       }, time*1000);  
     }
   }
